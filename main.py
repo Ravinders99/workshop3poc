b1e172579c46bd3bea08e04a512600005fd30299
Ravinder <ravindersingh62800@gmail.com>
2024-10-01 21:41:03 -0400

    first deploymnt

---
+++ main.py
@@ -0,0 +1,32 @@
+# import os, json, time
+
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+# from google.cloud import bigquery
+
+# @app.route("/", methods=['GET', 'POST'])
+# def hello_world():
+#     """Example Hello World route."""
+
+#     return f"Hello World!!!@@@@!!!"
+    
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Import Google Cloud BigQuery (if needed)
+from google.cloud import bigquery
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    """Example Hello World route."""
+    return f"Hello World!!!@@@@!!!"
+
+if __name__ == "__main__":
+    # For Azure App Service, use this format for port and host.
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
